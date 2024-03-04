a7b5c50bbb57aa92a077be5cb81a601bab22dcb1
Aiten <uuum1984@mail.ru>
2024-03-04 23:34:03 +0300

    9.4

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function show($id)
+    {
+        $posts = [
+            [
+                "id"=>1,
+                "name"=>"Имя поста",
+                "content"=>"Содержимое поста",
+                "date" => "Дата создания поста"
+            ],
+            [
+                "id"=>2,
+                "name"=>"Имя поста",
+                "content"=>"Содержимое поста",
+                "date" => "Дата создания поста"
+            ],
+            [
+                "id"=>3,
+                "name"=>"Имя поста",
+                "content"=>"Содержимое поста",
+                "date" => "Дата создания поста"
+            ],
+        ];
+        return $posts[$id-1];  
+    } 
+    public function index()
+    {
+        $posts = [
+            [
+                "id"=>1,
+                "name"=>"Имя поста",
+                "content"=>"Содержимое поста",
+                "date" => "Дата создания поста"
+            ],
+            [
+                "id"=>2,
+                "name"=>"Имя поста",
+                "content"=>"Содержимое поста",
+                "date" => "Дата создания поста"
+            ],
+            [
+                "id"=>3,
+                "name"=>"Имя поста",
+                "content"=>"Содержимое поста",
+                "date" => "Дата создания поста"
+            ],
+        ];
+        return $posts;  
+    }
+    public function store(Request $request){
+    return [
+        "store"=>true
+        ];
+    }
+    public function update(Request $request, $id){
+        return [
+            "update"=>true
+        ];
+    }
+    public function destroy($id){
+        return [
+            "destroy"=>true
+        ];
+    }
+}
